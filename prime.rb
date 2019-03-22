de8a512922b407469ca1bb2cc733db213a5f1ccb
Spencer Lindemuth <lindemut@gmail.com>
2019-03-22 01:26:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,21 @@
 # Add  code here!
 def prime?(num)
+test_array = []
 range = 2..(num/2)
   if num == 2
-    true
+    return true
   elsif num == 3
-    true
+    return true
   elsif num % 2 == 0
-    false
+    return false
   elsif num % 3 == 0
-    false
+    return false
   end
   range.each do |x|
-
+    if num % x != 0
+      test_array << true
+    else
+      test_array << false
 
 
 
